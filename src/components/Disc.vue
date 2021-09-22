a36f50865b42415d422a9aa5d00c97f740da2bec
Dario <dario.defrancisci92@gmail.com>
2021-09-22 13:22:33 +0200

    creato scaffolding

---
+++ src/components/Disc.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'Disc',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
